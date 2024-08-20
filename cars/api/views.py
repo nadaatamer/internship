077e375d29daf73de5a486a66ba61e5a4dff61b9
nadaa <nada.abdrabou@student.guc.edu.eg>
2024-08-21 01:56:10 +0300

    response msgs

--- cars/api/views.py
+++ cars/api/views.py
@@ -10,12 +10,15 @@ class CarViewSet(viewsets.ModelViewSet):
     def delete(self, request, *args, **kwargs):
         instance = self.get_object() #retrieves car instance i need to delete based on url parameters
         self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT) #deletion succeeded 
+        return Response({"message": "Car deleted "},status=status.HTTP_204_NO_CONTENT) #deletion succeeded 
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True) #many=True shows that multiple objects are being serialized
-        return Response(serializer.data)
+        return Response({
+            "message": "List of all cars",
+            "data": serializer.data
+        })
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False) #Checks if the update is partial (PATCH) or full (PUT).
@@ -25,4 +28,12 @@ class CarViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(instance, data=request.data, partial=partial) #binding incoming data to retrieved car instance
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
-        return Response(serializer.data)
+        return Response({
+            "message": "Car updated",
+            "data": serializer.data
+        })
+    
+    @action(detail=True, methods=['patch'])
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
