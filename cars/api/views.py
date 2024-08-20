9a9e3088b694f5a8b36ea06ad23c3441781b10f9
nadaa <nada.abdrabou@student.guc.edu.eg>
2024-08-21 02:01:42 +0300

    changes

--- cars/api/views.py
+++ cars/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from cars.models import Car
 from .serializers import CarSerializer
 
